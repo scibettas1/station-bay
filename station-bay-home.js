043df2449b82d910c955c1d738d0a7e422b01279
Samantha Scibetta <reception@Millenium.local>
2021-08-24 15:35:27 -0400

    continued with homepage css. vidoe background works now that it's hosted in godaddy. decided to use google fonts instead of trying to embed a custom font in rent cafe

---
+++ station-bay-home.js
@@ -0,0 +1,3 @@
+$('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source src="https://bnerealestate.com/site/assets/files/images/dest/SB_Pattern_02.mp4" type="video/mp4"></video></div>');
+
+$('.tag-line').append('<div><p class="tag2">DESTINATION:</p><p class="tag1">EXTRAORDINARY</p><p class="tag3">New Studio, 1 & 2 Bedroom Waterfront Rentals in South Amboy, NJ</p></div>');
