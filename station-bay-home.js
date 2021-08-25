0723cd407df0af076800ac093f9658af75496db1
Samantha Scibetta <reception@Millenium.local>
2021-08-25 13:50:00 -0400

    css for the romance section and the amenites section. organized and condensed css.

--- station-bay-home.js
+++ station-bay-home.js
@@ -2,5 +2,12 @@ $('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source
 
 $('.tag-line').append('<div><p class="tag2">DESTINATION:</p><p class="tag1">EXTRAORDINARY</p><p class="tag3">New Studio, 1 & 2 Bedroom Waterfront Rentals in South Amboy, NJ</p></div>');
 
-$('.tag-line-wrapper').append('<div><i class="fa-solid fa-arrow-down-long"></i></div>');
+// $('.tag-line-wrapper').append('<div><i class="fa-solid fa-arrow-down-long"></i></div>');
 
+$('.amenities-section .section-sub-header').append('<div><p class="sub-head">THE JOURNEY</p><br /></div>');
+
+// replace Sation Bay with ARRIVE AT
+$('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'ARRIVE AT'); });
+$(document).ajaxComplete(function () {
+  $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'ARRIVE AT'); });
+});
