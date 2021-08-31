b4e0c111f4556293865949af2652e5e09add5a07
Samantha Scibetta <reception@Millenium.local>
2021-08-31 16:35:43 -0400

    resident portal and online leasing complete. fixed button and phone number on contact page

---
+++ station-bay-portals.js
@@ -0,0 +1,12 @@
+$('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source src="https://bnerealestate.com/site/assets/files/images/dest/SB_Pattern_02.mp4" type="video/mp4"></video></div>');
+
+$('.tag-line').append('<div><p class="tag2">SET YOUR SIGHTS</p><p class="tag1">ON THE HORIZON</p></div>');
+
+// gives some space between the heading and the content
+$('#RentCafeContent h1').append('<div><br /><div>');
+
+const currentYear = new Date().getFullYear();
+console.log(currentYear)
+
+$('.copyright').append('<div id="new-copyright"><br /> © ' + currentYear + ' BNE Real Estate Group.  All Rights Reserved.<br />Web Design by <a href="https://scibettas1.github.io/#/" target="_blank">Sam Scibetta</a><div>');
+
