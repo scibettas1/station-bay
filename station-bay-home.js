7837f77c090aeb42373203588159a3e0026f7650
Samantha Scibetta <sammieeccentric@gmail.com>
2021-11-09 17:31:16 -0500

    adjusted the home page and fixed the anchor. trying a new style without any gaps between images

--- station-bay-home.js
+++ station-bay-home.js
@@ -1,18 +1,29 @@
-$('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source src="https://bnerealestate.com/site/assets/files/images/dest/SB_Pattern_02.mp4" type="video/mp4"></video></div>');
+$('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source src="https://bnerealestate.com/site/assets/files/images/dest/SB_Pattern_02.mp4" type="video/mp4"></video></div>')
+
 
 $('.tag-line').append('<div><p class="tag2">DESTINATION:</p><p class="tag1">EXTRAORDINARY</p><p class="tag3">New Studio, 1 & 2 Bedroom Waterfront Rentals in South Amboy, NJ</p></div>');
 
 $('.tag-line-wrapper').append('<div><a href="#romance"><img class="arrow animated bounce" src="https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/arrow-circle.png?w=100&h=100" /></a></div>');
 
-$('.top-slider-wrapper').append('<div id="romance"></div>');
+// $('.top-slider-wrapper').append('<div id="romance"></div>');
 
+$('.romance-para-section').prepend('<div class="anchor"><span id="romance"></span></div>');
+
+
+$('.amenities-section .content-wrapper').prepend('<div><h1>FEATURES & AMENITIES</h1><br /></div>');
 $('.amenities-section .section-sub-header').append('<div><p class="sub-head">THE JOURNEY</p><br /></div>');
 
+$('.neighbourhood-section .wrapper').prepend('<div><h1>NEIGHBORHOOD</h1><br /></div>');
 $('.neighbourhood-section .section-sub-header').append('<div><p class="sub-head">BRAND NEW DAY</p><br /></div>');
 
+$('.stylish-homes-section .custom-scroll-content').prepend('<div><h1>RESIDENCES</h1><br /></div>');
 $('.stylish-homes-section .section-sub-header').append('<div><p class="sub-head">INCREDIBLE LIVING</p><br /></div>');
 
-// replace Sation Bay with THE WATERFRONT
+$('.address-wrapper .wrapper').prepend('<div><h1>CONTACT US</h1><br /></div>');
+$('.address-wrapper .section-sub-header').append('<div><p class="sub-head">FOR YOU</p></div>');
+
+
+// replace Station Bay with THE WATERFRONT
 $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'THE WATERFRONT'); });
 $(document).ajaxComplete(function () {
   $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'THE WATERFRONT'); });
