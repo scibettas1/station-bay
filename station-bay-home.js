a36b21320dd1413d98de665ab984538e17148031
Samantha Scibetta <sammieeccentric@gmail.com>
2021-12-09 13:07:52 -0500

    adjusted layout on home page. minor typos on inner pages. added incentive bar. etc.

--- station-bay-home.js
+++ station-bay-home.js
@@ -1,3 +1,4 @@
+$('.main-header').prepend('<div class="promo"><a href="#new-copyright">UP TO 2 MONTHS FREE**</a></div>')
 $('.main-header').append('<div><video id="video-bg" autoplay muted loop ><source src="https://bnerealestate.com/site/assets/files/images/dest/SB_Pattern_02.mp4" type="video/mp4"></video></div>')
 
 
@@ -5,23 +6,8 @@ $('.tag-line').append('<div><p class="tag2">DESTINATION:</p><p class="tag1">EXTR
 
 $('.tag-line-wrapper').append('<div><a href="#romance"><img class="arrow animated bounce" src="https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/arrow-circle.png?w=100&h=100" /></a></div>');
 
-// $('.top-slider-wrapper').append('<div id="romance"></div>');
 
 $('.romance-para-section').prepend('<div class="anchor"><span id="romance"></span></div>');
-
-
-$('.amenities-section .content-wrapper').prepend('<div><h1>FEATURES & AMENITIES</h1><br /></div>');
-$('.amenities-section .section-sub-header').append('<div><p class="sub-head">THE JOURNEY</p><br /></div>');
-
-$('.neighbourhood-section .wrapper').prepend('<div><h1>NEIGHBORHOOD</h1><br /></div>');
-$('.neighbourhood-section .section-sub-header').append('<div><p class="sub-head">BRAND NEW DAY</p><br /></div>');
-
-$('.stylish-homes-section .custom-scroll-content').prepend('<div><h1>RESIDENCES</h1><br /></div>');
-$('.stylish-homes-section .section-sub-header').append('<div><p class="sub-head">INCREDIBLE LIVING</p><br /></div>');
-
-$('.address-wrapper .wrapper').prepend('<div><h1>CONTACT US</h1><br /></div>');
-$('.address-wrapper .section-sub-header').append('<div><p class="sub-head">FOR YOU</p></div>');
-
 
 // replace Station Bay with THE WATERFRONT
 $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'THE WATERFRONT'); });
@@ -29,15 +15,30 @@ $(document).ajaxComplete(function () {
   $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('Station Bay', 'THE WATERFRONT'); });
 });
 
+$('.amenities-section .content-wrapper').prepend('<div><h1>FEATURES & AMENITIES</h1><br /></div>');
+$('.amenities-section .section-sub-header').append('<div><p class="sub-head">THE JOURNEY</p><br /></div>');
+
+$('.neighbourhood-section .wrapper').prepend('<div><h1>RESIDENCES</h1><br /></div>');
+$('.neighbourhood-section .section-sub-header').append('<div><p class="sub-head">INCREDIBLE LIVING</p></div>');
+$('.neighbourhood-section p.section-para').append('<div class="btn-wrapper"><a href="floorplans.aspx"><div class="btn btn-primary"></div></a></div>');
+
+$('.stylish-homes-section .custom-scroll-content').prepend('<div><h1>NEIGHBORHOOD</h1><br /></div>');
+$('.stylish-homes-section .section-sub-header').append('<div><p class="sub-head">BRAND NEW DAY</p><br /></div>');
+$('.stylish-homes-section p.para-2').append('<div class="btn-wrapper show"><a href="neighborhood.aspx"><div class="btn btn-primary"></div></a></div>');
+
+$('.address-wrapper .wrapper').prepend('<div><h1>CONTACT US</h1><br /></div>');
+$('.address-wrapper .section-sub-header').append('<div><p class="sub-head">FOR YOU</p></div>');
+$('.address-wrapper #address').append('<div class="addy"><a href="https://www.google.com/maps/place/Radford+Ferry+Rd,+South+Amboy,+NJ+08879/@40.488462,-74.2855273,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3ca48c5762d01:0x649b7de82933dfda!8m2!3d40.488462!4d-74.2833386" target="_blank">1000 Schindler Drive (Located off Radford Ferry Road)<br/>South Amboy, NJ 08879</a></div>');
+
 
 const currentYear = new Date().getFullYear();
 console.log(currentYear)
 
-$('.copyright').append('<div id="new-copyright"><br /> © ' + currentYear + ' BNE Real Estate Group.  All Rights Reserved.<br />Web Design by <a href="https://scibettas1.github.io/#/" target="_blank">Sam Scibetta</a><div>');
+$('.copyright').append('<div id="new-copyright"><br />“*Incentives are based on a 26 month lease term and may be valid on select homes.<br />Additional incentives may be available. Prices and promotions are subject to change<br />at any time. Please contact the leasing office for full details.<br /><br /> © ' + currentYear + ' BNE Real Estate Group.  All Rights Reserved.<br />Web Design by <a href="https://scibettas1.github.io/#/" target="_blank">Sam Scibetta</a><div>');
 
 
 
-$('#map-canvas').append('<div id="map"><iframe width="100%" height="450" frameborder="0" style="border:0" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoBkPWmTk-mrqAFR6s5ptuiW87wiuryCI&q=place_id:ChIJpeRtzuPNw4kRjBdUV1UsENM" allowfullscreen></iframe></div>');
+$('#map-canvas').append('<div id="map"><iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/place/Station+Bay+at+South+Amboy/@40.4862833,-74.2820511,17z/data=!4m13!1m7!3m6!1s0x89c3ca48c5762d01:0x649b7de82933dfda!2sRadford+Ferry+Rd,+South+Amboy,+NJ+08879!3b1!8m2!3d40.488462!4d-74.2833386!3m4!1s0x89c3cb46849d5895:0x99d4d2d01e519947!8m2!3d40.4862442!4d-74.2802702" allowfullscreen></iframe></div>');
 
 function initMap() {
   const myLatLng = { lat: 40.4900694, lng: -74.2817293 };
